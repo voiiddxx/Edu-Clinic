c8e4427987838b2840920549e952b47c783bf4d2
void <95859137+voiiddxx@users.noreply.github.com>
2024-02-17 21:57:47 +0530

    Folder -- Designed folder structure -- Shadcn -- Again Installed shadcn

---
+++ app/(root)/student/auth/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='h-screen w-full flex'>
+      <div className='w-1/2 h-full bg-yellow-400'>
+      </div>
+      <div className='w-1/2 h-full flex flex-col justify-center items-center'>
+        <p className='font-semibold text-xl text-blue-700'>EduClinic</p>
+        <p className='font-semibold text-2xl mt-2 text-zinc-800'>Welcome Back!</p>
+        <p className='text-sm mt-2 text-zinc-500'>Please Enter Your Details</p>
+       
+
+      </div>
+    </div>
+  )
+}
+
+export default page
