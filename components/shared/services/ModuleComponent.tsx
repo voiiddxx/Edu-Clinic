2843aba124af94497759ffc1d7106cb2838763b9
void <95859137+voiiddxx@users.noreply.github.com>
2024-02-24 23:48:50 +0530

    Some changes

--- components/shared/services/ModuleComponent.tsx
+++ components/shared/services/ModuleComponent.tsx
@@ -12,7 +12,7 @@ const ModuleComponent = ({serviceId} : moduleComponentProps) => {
   return (
     <div className='w-full  pl-36  pr-36'>
       <div className='mt-5 flex justify-between'>
-        <h1 className='text-xl text-zinc-900 font-bold' >Your Modules(5)</h1>
+        <h1 className='text-xl text-zinc-900 font-bold' >Your Modules</h1>
         <Button className='bg-zinc-800 hover:bg-blue-700 font-normal' >
             <Plus className='mr-2' size={15}/> 
              <ModuleForm id={serviceId} type='ADD' />
