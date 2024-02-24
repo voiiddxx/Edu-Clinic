eec23de1ad06f78f72d431a67ea2143affd0016d
void <95859137+voiiddxx@users.noreply.github.com>
2024-02-25 02:12:48 +0530

    Implemted ui for project card and project details

---
+++ app/(root)/serviceprovider/projects/[id]/page.tsx
@@ -0,0 +1,72 @@
+import OrgNav from '@/components/shared/services/OrgNav'
+import { Button } from '@/components/ui/button'
+import { getProjectWithId } from '@/lib/database/actions/project.action'
+import { Building, Building2, DollarSign, IndianRupee, Loader, Phone } from 'lucide-react'
+import Image from 'next/image'
+import React from 'react'
+
+const page = async ({
+    params:{ id },
+  } : {params:{
+    id : string
+  }}) => {
+
+
+    const data = await getProjectWithId({id:id}); 
+    console.log("this is the value if project response" , data);
+    
+  return (
+    <div>
+      <OrgNav/>
+      <div className='min-h-screen w-full px-28' >
+        <div className='h-28 w-full border-b flex flex-col justify-center    ' >
+            <h1 className='text-xl font-semibold text-zinc-800' >Metal Detector Project</h1>
+            <p className='text-zinc-500' >Explore all the project detail about the project</p>
+        </div>
+
+        <div className='w-full h-full flex mt-10' >
+            <div className='h-[400px] w-[500px] rounded-lg border-[1px] border-zinc-200 px-4 py-4' >
+                <div className='bg-zinc-600 h-full w-full rounded-md'>
+                        <Image className='h-full w-full object-cover' height={500} width={500} src={data.poster} alt='project poster image' />
+                </div>
+            </div>
+            <div className='ml-16 mt-4 w-[600px]'>
+                <h1 className='text-2xl font-semibold text-zinc-700' >{data.name}</h1>
+                <p className='text-sm mt-4 text-zinc-500' >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure rem voluptates aliquam laudantium blanditiis et sed inventore consequuntur possimus ab! Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo omnis id optio nam officiis. Laboriosam natus porro consequuntur at quasi. </p>
+                <div className='flex gap-2 mt-4 items-center' >
+                    <div className='h-14 w-14 rounded-full bg-slate-100 flex justify-center items-center'><Building2 className='text-indigo-700' />
+                    </div>
+                    <p>{data.college}</p>
+                    
+                </div>
+              <div className='flex gap-8' >
+              <div className='flex mt-6 ml-4 items-center gap-2 ' >
+                    <Loader className='text-indigo-600' size={17} />
+                    <p className='text-indigo-600' >Patent Granted</p>
+                </div>
+                <div className='flex mt-6 ml-4 items-center gap-2 ' >
+                    <Phone className='text-green-600' size={17} />
+                    <p className='text-green-600' >91+ 8056958421</p>
+                </div>
+              </div>
+
+              <div className='flex items-center gap-2 mt-10 ml-3' >
+                <IndianRupee/>
+                <h1 className='text-2xl font-semibold' >{data.ammount}</h1>
+              </div>
+
+              <div className='flex gap-2 mt-4'>
+              <Button variant={'outline'} className='w-full' >Connect through mail</Button>
+                <Button className='w-full bg-zinc-900' >Download project file</Button>
+                
+              </div>
+            </div>
+
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default page
--- app/(root)/serviceprovider/projects/page.tsx
+++ app/(root)/serviceprovider/projects/page.tsx
@@ -2,12 +2,12 @@ import OrgNav from '@/components/shared/services/OrgNav'
 import { Button } from '@/components/ui/button'
 import { getProject } from '@/lib/database/actions/project.action'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const page = async () => {
 
     const projects = await getProject();
-    console.log(projects);
     
 
   return (
@@ -22,7 +22,8 @@ const page = async () => {
                 projects.length < 1 ? <div></div> : <div className='flex flex-wrap gap-4 mt-8' >
                     {
                         projects.map((curr : any)=>{
-                            return <div className='min-h-[250px] w-[500px] border-[1px] border-zinc-200 rounded-md flex px-2 py-2 gap-4' >
+                            return <Link href={`/serviceprovider/projects/${curr._id}`}>
+                            <div className='min-h-[250px] w-[500px] border-[1px] border-zinc-200 rounded-md flex px-2 py-2 gap-4' >
                             <div className='bg-red-200 h-full w-[200px] rounded-sm' >
                                 <Image className='h-full w-[200px] object-cover' src={curr.poster} height={600} width={600} alt='projects' />
                             </div>
@@ -40,6 +41,7 @@ const page = async () => {
                            </div>
                             
                         </div>
+                            </Link>
                         })
                     }
                 </div>
