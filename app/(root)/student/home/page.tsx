f3ed00d523fd74326e66de9c19117405eacacf41
lsvishawjeet <sidhuvishaw999@gmail.com>
2024-03-11 19:33:38 +0530

    fixed home page

--- app/(root)/student/home/page.tsx
+++ app/(root)/student/home/page.tsx
@@ -1,29 +1,27 @@
-import StudentHero from '@/components/shared/student/StudentHero'
-import StudentModule from '@/components/shared/student/StudentModule'
-import StudentNav from '@/components/shared/student/StudentNav'
-import StudentOrg from '@/components/shared/student/StudentOrg'
-import StudentServices from '@/components/shared/student/StudentServices'
-import { getAllModule } from '@/lib/database/actions/module.action'
-import { getAllOrganization } from '@/lib/database/actions/organization.auth.action'
-import {  getAllServices } from '@/lib/database/actions/service.action'
-import React from 'react'
+import StudentHero from "@/components/shared/student/StudentHero";
+import StudentModule from "@/components/shared/student/StudentModule";
+import StudentNav from "@/components/shared/student/StudentNav";
+import StudentOrg from "@/components/shared/student/StudentOrg";
+import StudentServices from "@/components/shared/student/StudentServices";
+import { getAllModule } from "@/lib/database/actions/module.action";
+import { getAllOrganization } from "@/lib/database/actions/organization.auth.action";
+import { getAllServices } from "@/lib/database/actions/service.action";
+import React from "react";
 
 const page = async () => {
   const modules = await getAllModule();
   const allServices = await getAllServices();
   const orgs = await getAllOrganization();
-  
-  
-    
+
   return (
     <div>
-      <StudentNav/>
-      <StudentHero/>
-      <StudentOrg organization={orgs} />
-      <StudentServices services={allServices} />
-      <StudentModule allModule={modules} />
+      <StudentNav />
+      <StudentHero />
+      {/* <StudentOrg organization={orgs} /> */}
+      {/* <StudentServices services={allServices} /> */}
+      {/* <StudentModule allModule={modules} /> */}
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
