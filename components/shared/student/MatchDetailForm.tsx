928f2d9d8c92d3e11db31c3728c937db7b906519
lsvishawjeet <sidhuvishaw999@gmail.com>
2024-03-15 21:34:13 +0530

    nothing serious

--- components/shared/student/MatchDetailForm.tsx
+++ components/shared/student/MatchDetailForm.tsx
@@ -1,9 +1,9 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import ServiceOptionObject from './MatchDetailFormComponents/ServiceOptionObject';
+import ServiceOptionObject from "./MatchDetailFormComponents/ServiceOptionObject";
 import UpskillingForm from "./MatchDetailFormComponents/UpskillingForm";
 import StudentModule from "./StudentModule";
-import { getAllModule } from '@/lib/database/actions/module.action';
+import { getAllModule } from "@/lib/database/actions/module.action";
 import FilteredModules from "./FilteredModules";
 import CompetitiveExamForm from "./MatchDetailFormComponents/CompetitiveExamForm";
 
@@ -15,7 +15,7 @@ function MatchDetailForm() {
     const fetchData = async () => {
       const modules = await getAllModule();
       setAllModules(modules);
-      console.log(modules)
+      console.log(modules);
     };
 
     fetchData();
@@ -23,20 +23,22 @@ function MatchDetailForm() {
 
   const renderForm = () => {
     if (option === 0) {
-      return <>
-      <UpskillingForm />
-      </>
+      return (
+        <>
+          <UpskillingForm />
+        </>
+      );
     } else if (option === 1) {
-      return <CompetitiveExamForm/>;
+      return <CompetitiveExamForm />;
     }
-    console.log("Testing")
+    console.log("Testing");
   };
 
   return (
     <div className="h-auto flex bg-slate-50 justify-center items-center ">
       <div className="sm:w-full ">
-        <div className="grid grid-cols-12  rounded-lg">
-          <div className="col-span-3 pt-4 p-2 sticky top-0">
+        <div className="sm:grid grid-cols-12 rounded-lg">
+          <div className="md:col-span-3 md:pt-4 md:p-2 md:sticky top-0 ">
             {ServiceOptionObject.map((item, id) => (
               <p
                 key={id}
@@ -51,9 +53,7 @@ function MatchDetailForm() {
               </p>
             ))}
           </div>
-          <div className="col-span-9 p-8 h-full ">
-            {renderForm()}
-          </div>
+          <div className="col-span-9 p-8 h-full ">{renderForm()}</div>
         </div>
       </div>
     </div>
--- components/shared/student/StudentOrg.tsx
+++ components/shared/student/StudentOrg.tsx
@@ -1,53 +1,58 @@
-"use client"
+"use client";
 
-import { IOrganization } from '@/lib/database/models/serviceprovider.model'
-import { Flame} from 'lucide-react'
-import Image from 'next/image'
-import { useRouter } from 'next/navigation'
-import React from 'react'
-
-
-
-
+import { IOrganization } from "@/lib/database/models/serviceprovider.model";
+import { Flame } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 type StudentOrgsParams = {
-  organization:any
-}
-const StudentOrg = ({organization} : StudentOrgsParams) => {
-  
+  organization: any;
+};
+const StudentOrg = ({ organization }: StudentOrgsParams) => {
   const router = useRouter();
-  
-  
-  return (
-    <div className='pb-20 w-full px-40' >
-      <div className='flex gap-2 items-center' >
-      <Flame/>
-      <h1 className='text-xl font-semibold text-zinc-800' >  Explore Organizations</h1>
-      </div>
-      <p className='text-zinc-500 text-sm mt-2' >You Can explore all organization out here and explore their services</p>
-      {
-        organization.length > 1 && (
-            <div className='flex gap-4 flex-wrap mt-8'>
-                {
-                    organization.map((curr:IOrganization)=>{
-                        return <div onClick={()=>{
-                          router.push(`/student/home/organization/${curr._id}`)
-                        }} className='flex flex-col gap-2 items-center justify-center cursor-pointer' >
-                            <div className='h-28 w-28 rounded-full bg-blue-900' >
-                              <Image className='h-28 w-28 rounded-full object-cover' src={curr.orgImage} height={400} width={500} alt='orgImage' />
-                            </div>
-                            <div className='w-40'>
-                            <p className='text-center' >{curr.orgName}</p>
-                            </div>
-                        </div>
-                    })
-                }
-            </div>
-        )
-      }
-    </div>
-    
-  )
-}
 
-export default StudentOrg
+  return (
+    <div className="pb-20 w-full px-40">
+      <div className="flex gap-2 items-center">
+        <Flame />
+        <h1 className="text-xl font-semibold text-zinc-800">
+          {" "}
+          Explore Organizations
+        </h1>
+      </div>
+      <p className="text-zinc-500 text-sm mt-2">
+        You Can explore all organization out here and explore their services
+      </p>
+      {organization.length > 1 && (
+        <div className="flex gap-4 flex-wrap mt-8">
+          {organization.map((curr: IOrganization) => {
+            return (
+              <div
+                onClick={() => {
+                  router.push(`/student/home/organization/${curr._id}`);
+                }}
+                className="flex flex-col gap-2 items-center justify-center cursor-pointer"
+              >
+                <div className="h-28 w-28 rounded-full bg-blue-900">
+                  <Image
+                    className="h-28 w-28 rounded-full object-cover"
+                    src={curr.orgImage}
+                    height={400}
+                    width={500}
+                    alt="orgImage"
+                  />
+                </div>
+                <div className="w-40">
+                  <p className="text-center">{curr.orgName}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default StudentOrg;
