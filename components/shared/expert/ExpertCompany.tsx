55a9ad9c0ee0c837845dd517353825af30abab87
void <95859137+voiiddxx@users.noreply.github.com>
2024-03-21 21:57:04 +0530

    Soretd issues

--- components/shared/expert/ExpertCompany.tsx
+++ components/shared/expert/ExpertCompany.tsx
@@ -6,6 +6,8 @@ type expertCompanyProps = {
     orgs:any
 }
 const ExpertCompany = ({orgs} : expertCompanyProps) => {
+
+  
   return (
     <div className='min-h-screen w-full' >
       <div className='h-16 w-full border-b flex flex-col justify-center' >
@@ -15,13 +17,13 @@ const ExpertCompany = ({orgs} : expertCompanyProps) => {
 
       <div className='w-full flex flex-wrap gap-4 mt-5' >
        {
-        orgs.length < 1 ? <div className='min-h-screen w-full flex  justify-center items-start pt-36' >
+        orgs.length < 0 ? <div className='min-h-screen w-full flex  justify-center items-start pt-36' >
           <div className='h-40 w-96 bg-slate-200 rounded flex flex-col justify-center items-center' >
             <h1 className='text-xl font-semibold text-zinc-800' >No Data Found</h1>
             <p className='text-zinc-600 text-sm mt-1' >Sorry No Data found, Please try again later</p>
 
           </div>
-        </div> : <div>
+        </div> : <div className='flex flex-col gap-2' >
           {
         orgs.map((curr:any)=>{
             return <OrgCard orgData={curr} key={curr._id} />
