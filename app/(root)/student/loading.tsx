dafbc427e9b35c65ad8114ecf68411d5a34d9beb
void <95859137+voiiddxx@users.noreply.github.com>
2024-04-09 12:33:19 +0530

    Added loading componenet for student , service provider and for expert pannel

---
+++ app/(root)/student/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='min-h-screen w-full bg-white flex justify-center items-center' >
+        <p className='text-xl text-zinc-800' >Please wait for a while...</p>
+      
+    </div>
+  )
+}
+
+export default loading
