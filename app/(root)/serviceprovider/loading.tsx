38d396b9f83c928e219201199c9444ddca7fc9a8
lsvishawjeet <sidhuvishaw999@gmail.com>
2024-05-06 08:15:49 +0530

    loading in org dashboard

--- app/(root)/serviceprovider/loading.tsx
+++ app/(root)/serviceprovider/loading.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const loading = () => {
   return (
     <div className='min-h-screen w-full bg-white flex justify-center items-center' >
-        <p className='text-xl text-zinc-800' >Please wait for a while...</p>
-
+         <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-zinc-500"></div>
+      
     </div>
   )
 }
